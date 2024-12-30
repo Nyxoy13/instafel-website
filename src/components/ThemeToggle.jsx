6d34f91dc26dd331458229696625b3fc3140151a
Nyxoy13 <118988269+Nyxoy13@users.noreply.github.com>
2024-12-31 02:13:01 +0300

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTheme } from '../hooks/useTheme';
+import { useTheme } from '../src/useTheme';
 
 export function ThemeToggle() {
   const { isDark, toggleTheme } = useTheme();
