e758d9f623b1cd741eef89532117e91d00bb41f2
Nyxoy13 <118988269+Nyxoy13@users.noreply.github.com>
2024-12-30 21:54:34 +0300

    Update ifl.jsx

--- src/components/ifl.jsx
+++ src/components/ifl.jsx
@@ -35,7 +35,7 @@ export function LoadingBar() {
 export function Footer() {
   return (
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-      <p className="text-xs text-muted-foreground">Developed by mamiiblt with ❤️</p>
+      <p className="text-xs text-muted-foreground">ModderByTR Tarafından Geliştirildi ❤️</p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
         <Link href="https://t.me/instaflax" className="text-xs hover:underline underline-offset-4" prefetch={false}>
           Telegram Kanalı
