c66718f636dac575106eb8a95e374a70f9b556eb
Nyxoy13 <118988269+Nyxoy13@users.noreply.github.com>
2024-12-31 02:30:10 +0300

    Update ifl.jsx

--- src/components/ifl.jsx
+++ src/components/ifl.jsx
@@ -15,9 +15,9 @@ export function Header() {
           </Link>
           <Link href="/library_backup" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
             Kütüphane
-<img width="24" height="24" src="https://img.icons8.com/material-rounded/24/download--v1.png" alt="download--v1"/>
           </Link>
-          <Link href="/download?version=latest" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+          <Link href="/download?version=latest" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false
+<img width="24" height="24" src="https://img.icons8.com/material-rounded/24/download--v1.png" alt="download--v1"/>}>
             indir
           </Link>
         </nav>
